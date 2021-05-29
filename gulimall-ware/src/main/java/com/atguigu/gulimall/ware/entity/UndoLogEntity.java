24dd7f7ce52daf19db038e59402491b05e13535e
Luwei Deng <63690456+LostRed@users.noreply.github.com>
2021-05-29 21:58:33 +0800

    配置nacos

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/entity/UndoLogEntity.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/entity/UndoLogEntity.java
@@ -2,14 +2,14 @@ package com.atguigu.gulimall.ware.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.util.Date;
-import lombok.Data;
 
 /**
- * 
- * 
+ *
+ *
  * @author lostred
  * @email lostred@outlook.com
  * @date 2021-05-29 17:54:32
@@ -20,40 +20,40 @@ public class UndoLogEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * 
+	 *
 	 */
 	@TableId
 	private Long id;
 	/**
-	 * 
+	 *
 	 */
 	private Long branchId;
 	/**
-	 * 
+	 *
 	 */
 	private String xid;
 	/**
-	 * 
+	 *
 	 */
 	private String context;
 	/**
-	 * 
+	 *
 	 */
-	private Longblob rollbackInfo;
+	private String rollbackInfo;
 	/**
-	 * 
+	 *
 	 */
 	private Integer logStatus;
 	/**
-	 * 
+	 *
 	 */
 	private Date logCreated;
 	/**
-	 * 
+	 *
 	 */
 	private Date logModified;
 	/**
-	 * 
+	 *
 	 */
 	private String ext;
 
