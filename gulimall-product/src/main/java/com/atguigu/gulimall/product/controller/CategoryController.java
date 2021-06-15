441078b2202f7f325c9e42e4b9b6b44c5ce30b70
Luwei Deng <63690456+LostRed@users.noreply.github.com>
2021-06-15 08:51:47 +0800

    分类更新拖拽修改顺序、更新品牌新增和修改

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Map;
 
 
-
 /**
  * 商品三级分类
  *
@@ -29,8 +28,8 @@ public class CategoryController {
      * 查出所有分类以及子分类，并以树形结构组装起来
      */
     @RequestMapping("/list/tree")
-    public R list(@RequestParam Map<String, Object> params){
-        List<CategoryEntity> entities=categoryService.listWithTree();
+    public R list(@RequestParam Map<String, Object> params) {
+        List<CategoryEntity> entities = categoryService.listWithTree();
         return R.ok().put("data", entities);
     }
 
@@ -39,18 +38,18 @@ public class CategoryController {
      * 信息
      */
     @RequestMapping("/info/{catId}")
-    public R info(@PathVariable("catId") Long catId){
-		CategoryEntity category = categoryService.getById(catId);
+    public R info(@PathVariable("catId") Long catId) {
+        CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
 
     /**
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@RequestBody CategoryEntity category){
-		categoryService.save(category);
+    public R save(@RequestBody CategoryEntity category) {
+        categoryService.save(category);
 
         return R.ok();
     }
@@ -59,8 +58,8 @@ public class CategoryController {
      * 修改
      */
     @RequestMapping("/update")
-    public R update(@RequestBody CategoryEntity category){
-		categoryService.updateById(category);
+    public R update(@RequestBody CategoryEntity category) {
+        categoryService.updateById(category);
 
         return R.ok();
     }
@@ -69,9 +68,18 @@ public class CategoryController {
      * 删除
      */
     @RequestMapping("/delete")
-    public R delete(@RequestBody Long[] catIds){
+    public R delete(@RequestBody Long[] catIds) {
 //		categoryService.removeByIds(Arrays.asList(catIds));
-		categoryService.removeMenuByIds(Arrays.asList(catIds));
+        categoryService.removeMenuByIds(Arrays.asList(catIds));
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update/sort")
+    public R updateSort(@RequestBody CategoryEntity[] categories) {
+        categoryService.updateBatchById(Arrays.asList(categories));
         return R.ok();
     }
 
